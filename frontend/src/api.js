e1a06c2001aaee106dbb6ec5164705e53680c973
Dean Pawiro <deanpawiro11@gmail.com>
2026-02-23 19:25:35 -0300

    Configure frontend to use REACT_APP_API_URL for backend API calls

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+
+const API_URL = process.env.REACT_APP_API_URL;
 const API = axios.create({
-	baseURL: process.env.REACT_APP_BACKEND_URL || "http://localhost:5000/api",
+	baseURL: API_URL,
 	headers: {
 		"Content-Type": "application/json",
 	},
